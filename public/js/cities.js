80eea7c8ebe15633deb2bd0bda0281fb5081da5d
Appoline Cogan <appolinecogan@gmail.com>
2022-09-08 12:55:21 +0200

    added handlers for buying and selling items

--- public/js/cities.js
+++ public/js/cities.js
@@ -1,18 +1,43 @@
-const getCityHandler = async (event) => {
+const buyItemHandler = async (event) => {
   if (event.target.hasAttribute("data-id")) {
-    const cityId = event.target.getAttribute("data-id");
+    const itemId = event.target.getAttribute("data-id");
 
-    const response = await fetch(`/api/cities/${cityId}`, {
-      method: "GET",
+    //route to fill out
+    const response = await fetch(`/${itemId}`, {
+      method: "POST",
+      body: JSON.stringify({ itemId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     //route to fill out
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("Failed to load the city!");
+      alert(response.statusText);
     }
   }
 };
 
-document.querySelector(".cities").addEventListener("click", getCityHandler);
+const sellItemHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const itemId = event.target.getAttribute("data-id");
+
+    //route to fill out
+    const response = await fetch(`/${itemId}`, {
+      method: "DELETE",
+    });
+
+    //route to fill out
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector(".buy-item").addEventListener("click", buyItemHandler);
+
+document.querySelector(".sell-item").addEventListener("click", sellItemHandler);
