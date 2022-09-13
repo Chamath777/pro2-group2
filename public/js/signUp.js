b5ef7cfca35104bdc9e257687dd3c5ce4fa0eacb
Appoline Cogan <appolinecogan@gmail.com>
2022-09-13 11:16:47 +0200

    fixed id on signup page

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,24 +1,22 @@
-async function SignupFormHandler(event)
-{
-	event.preventDefault();
+async function SignupFormHandler(event) {
+  event.preventDefault();
 
-	const name = document.querySelector('#name-signup').value.trim();
-	const email = document.querySelector('#email-signup').value.trim();
-	const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-	if (name && email && password) 
-	{
-		const response = await fetch('/api/user', 
-		{
-			method: 'POST',
-			body: JSON.stringify({ name, email, password }),
-			headers: { 'Content-Type': 'application/json' },
-		});
+  if (name && email && password) {
+    const response = await fetch("/api/user", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-		if (response.ok) document.location.replace('/saveFile');
-		else alert(response.statusText);
-	}
-};
+    if (response.ok) document.location.replace("/saveFile");
+    else alert(response.statusText);
+  }
+}
 
-document.querySelector('.signup-form')
-  		.addEventListener('submit', SignupFormHandler);
+document
+  .querySelector(".signup-form-btn")
+  .addEventListener("submit", SignupFormHandler);
