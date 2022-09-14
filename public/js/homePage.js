f10f78572d0b920155f81d47a554cf5167b92bdd
Appoline Cogan <appolinecogan@gmail.com>
2022-09-14 10:21:58 +0200

    changed columns sizes and wrote code to make burger menu clickable on navbar

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -5,3 +5,17 @@ function goToHomePageHandler() {
 document
   .querySelector("#homepage")
   .addEventListener("click", goToHomePageHandler);
+
+//this is to make the navbar burger button clickable
+document.addEventListener("DOMContentLoaded", () => {
+  // Get  "navbar-burger" elements
+  const navbarBurger = document.querySelector(".navbar-burger");
+
+  const navBarMenu = document.querySelector(".navbar-menu");
+  // Add a click event on each of them
+  navbarBurger.addEventListener("click", () => {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    navbarBurger.classList.toggle("is-active");
+    navBarMenu.classList.toggle("is-active");
+  });
+});
