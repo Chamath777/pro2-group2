93065bad3aadc32e70fcca72c364422a7375f57f
Wombattree <63911123+Wombattree@users.noreply.github.com>
2022-09-18 23:07:33 +0930

    Trying to get deployment working

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,28 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
+const sequelize = new Sequelize
+(
+  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: 
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      decimalNumbers: true,
+    },
+  }
+);
+
+    // const sequelize = process.env.JAWSDB_URL ? 
+    // new Sequelize(process.env.JAWSDB_URL) : 
+    // new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
+    // {
+    //   host: 'localhost',
+    //   dialect: 'mysql',
+    //   dialectOptions: {
+    //     decimalNumbers: true,
+    //   },
+    // });
 
 module.exports = sequelize;
