e42b47bdf4177c779e60b637fe1b0de647f281df
Wombattree <63911123+Wombattree@users.noreply.github.com>
2022-09-18 23:33:47 +0930

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,28 +2,54 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize
-(
-  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: 
+let sequelize;
+if (process.env.NODE_ENV === "testing")
+{
+  sequelize = new Sequelize
+  (
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
     {
-      decimalNumbers: true,
-    },
-  }
-);
-
-    // const sequelize = process.env.JAWSDB_URL ? 
-    // new Sequelize(process.env.JAWSDB_URL) : 
-    // new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
-    // {
-    //   host: 'localhost',
-    //   dialect: 'mysql',
-    //   dialectOptions: {
-    //     decimalNumbers: true,
-    //   },
-    // });
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: 
+      {
+        decimalNumbers: true,
+      },
+    }
+  );
+}
+else
+{
+  sequelize = new Sequelize
+  (
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+      host: 'ascrivener-merchant-game.herokuapp.com',
+      dialect: 'mysql',
+      dialectOptions: 
+      {
+        decimalNumbers: true,
+      },
+    }
+  );
+}
+// const sequelize = new Sequelize
+// (
+//   process.env.DB_NAME, 
+//   process.env.DB_USER, 
+//   process.env.DB_PASSWORD, 
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     dialectOptions: 
+//     {
+//       decimalNumbers: true,
+//     },
+//   }
+// );
 
 module.exports = sequelize;
